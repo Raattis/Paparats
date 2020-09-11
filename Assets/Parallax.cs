6f5b4d43feede7abdce02ba25a30ee9712f7c57e
Raattis <raattis@gmail.com>
2020-09-11 20:47:00 +0300

    Parallaxatiivi

---
+++ Assets/Parallax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public float parallaxScale = 1.0f;
+
+    Vector3 originalPos;
+    PlayerPosition playerPosition;
+
+    void Start()
+    {
+        playerPosition = GameObject.FindObjectOfType<PlayerPosition>();
+        originalPos = transform.localPosition;
+    }
+
+    void Update()
+    {
+        transform.localPosition = originalPos + new Vector3(-playerPosition.x, -playerPosition.y) * parallaxScale;
+    }
+}
---
+++ Assets/PlayerPosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPosition : MonoBehaviour
+{
+    [System.NonSerialized]
+    public float x = 0;
+    [System.NonSerialized]
+    public float y = 0;
+
+    public float minX = -1.0f;
+    public float maxX = 1.0f;
+    private Vector2 speed = new Vector2(3.0f, 1.0f);
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        x += Time.deltaTime * speed.x * Input.GetAxis("Horizontal");
+        y += Time.deltaTime * speed.y * Input.GetAxis("Vertical");
+    }
+}
