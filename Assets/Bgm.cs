685ff1085a472854f265af334ecb73cd8ce585eb
Raattis <raattis@gmail.com>
2020-09-13 13:11:29 +0300

    bgm

---
+++ Assets/Bgm.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bgm : MonoBehaviour
+{
+    void Awake()
+    {
+        bool notAlone = false;
+        foreach (Bgm bgm in FindObjectsOfType<Bgm>())
+        {
+            if (bgm != this)
+            {
+                notAlone = true;
+                break;
+            }
+        }
+
+        if (notAlone)
+        {
+            GameObject.Destroy(gameObject);
+            return;
+        }
+
+        GameObject.DontDestroyOnLoad(gameObject);   
+    }
+
+    void Update()
+    {
+        
+    }
+}
